627e5ae12c9b9c7ed81ee742b92c939513005f38
Denis Tisov <dinyat@yandex-team.ru>
2017-03-29 17:59:51 +0300

    small fix

--- src/main/java/ru/tisov/denis/platform/docker/impl/DockerClientFactoryImpl.java
+++ src/main/java/ru/tisov/denis/platform/docker/impl/DockerClientFactoryImpl.java
@@ -45,7 +45,7 @@ public class DockerClientFactoryImpl implements DockerClientFactory {
         Host host = hostService.getByName(hostName);
         DockerClientConfig config = DefaultDockerClientConfig.createDefaultConfigBuilder()
                 .withDockerHost("tcp://" + host.getUrl() + ":2376")
-                .withDockerCertPath("/Users/denis/.docker/machine/machines/" + hostName)
+                .withDockerCertPath(host.getHostPath())
                 .withApiVersion("1.12")
                 .withRegistryUrl(registryUrl)
                 .build();
