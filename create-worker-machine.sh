4164b3aab4f30f51226d150363d9401f7cbe8479
Denis Tisov <dinyat@yandex-team.ru>
2017-05-12 10:09:14 +0300

    скрипт создание swarm мастера

--- create-worker-machine.sh
+++ create-worker-machine.sh
@@ -4,6 +4,8 @@ docker-machine rm -f worker1
 docker-machine create \
     --driver virtualbox \
     --engine-insecure-registry 192.168.99.100:5000 \
-    worker1
+    swarm2
 
-docker swarm join-token worker1
+eval $(docker-machine env swarm2)
+docker swarm init --advertise-addr 192.168.99.103
+docker swarm join-token worker
