1d406963cec29ca62059118f4919b8295f8233fc
Denis Tisov <dinyat@yandex-team.ru>
2017-05-25 10:36:02 +0300

    скрипты

--- create-worker-machine.sh
+++ create-worker-machine.sh
@@ -7,5 +7,11 @@ docker-machine create \
     swarm2
 
 eval $(docker-machine env swarm2)
-docker swarm init --advertise-addr 192.168.99.103
+docker swarm init --advertise-addr 192.168.99.102
 docker swarm join-token worker
+
+docker swarm join \
+    --token SWMTKN-1-5itl0czy0e9r5gzwxm4u0w411u8kxc771833o2mza3elb8qisj-2atiml1y2kieccx5e8mu8pee6 \
+    192.168.99.102:2377
+
+docker-machine regenerate-certs worker1
